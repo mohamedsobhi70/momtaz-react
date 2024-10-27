6901cee8ee5d05c4a0a38fc7d18a8a36e1af25ca
mohamedsobhi70 <sobhi7070@gmail.com>
2024-10-27 21:29:22 +0300

    add some data

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -6,6 +6,18 @@ import teacher5 from "../assets/images/tacher-1.png"
 import teacher6 from "../assets/images/tacher-2.png"
 import teacher7 from "../assets/images/tacher-3.png"
 import teacher8 from "../assets/images/tacher-4.png"
+import teacher9 from "../assets/images/team-3.png"
+import teacher10 from "../assets/images/team-2.png"
+import teacher11 from "../assets/images/team-3.png"
+import teacher12 from "../assets/images/team-4.png"
+import teacher13 from "../assets/images/team-1.png"
+import teacher14 from "../assets/images/team-3.png"
+import teacher15 from "../assets/images/team-2.png"
+import teacher16 from "../assets/images/team-1.png"
+import teacher17 from "../assets/images/team-3.png"
+import teacher18 from "../assets/images/team-4.png"
+import teacher19 from "../assets/images/team-3.png"
+import teacher20 from "../assets/images/team-2.png"
 
 export const teachers = [
     {
@@ -33,7 +45,6 @@ export const teachers = [
         city: "الرياض",
         location: "التعاون",
         image: teacher2
-
     },
     {
         id: 3,
@@ -47,11 +58,10 @@ export const teachers = [
         city: "الرياض",
         location: "التعاون",
         image: teacher3
-
     },
     {
         id: 4,
-        name: 'أ/ عادل التهامي محمود',
+        name: 'أ/ أشرف محمد عبد المنعم',
         subjects: ["اللغة الأنجليزية", "العلوم", "التاريخ"],
         grades: ["الصف الخامس", "الصف السادس"],
         rate: 4.3,
@@ -61,12 +71,11 @@ export const teachers = [
         city: "الرياض",
         location: "التعاون",
         image: teacher4
-
     },
     {
         id: 5,
-        name: 'أ/ محمود عادل التهامي ',
-        subjects: ["اللغة الأنجليزية", "العلوم", "التاريخ"],
+        name: 'أ/ ابراهيم الحسينى عوده',
+        subjects: ["اللغة الأنجليزية", "الجغرافيا"],
         grades: ["الصف السادس", "الصف الثالث"],
         rate: 4.3,
         ratingno: 412,
@@ -75,13 +84,12 @@ export const teachers = [
         city: "الرياض",
         location: "التعاون",
         image: teacher5
-
     },
     {
         id: 6,
         name: 'أ/ صبحى عادل محمود',
-        subjects: ["اللغة الفرنسيه", "فيزياء", "التاريخ"],
-        grades: ["الصف الخامس", "الصف السابع"],
+        subjects: ["اللغة الفرنسية", "فيزياء"],
+        grades: ["الصف الثامن", "الصف الثانى"],
         rate: 3.1,
         ratingno: 241,
         gender: "ذكر",
@@ -92,8 +100,8 @@ export const teachers = [
     },
     {
         id: 7,
-        name: 'أ/ عاليا محمود السيد ',
-        subjects: ["اللغة الفرنسيه", "فيزياء", "التاريخ"],
+        name: 'أ/ صفاء محمد الحسينى',
+        subjects: ["فيزياء", "الكيمياء"],
         grades: ["الصف الخامس", "الصف السابع"],
         rate: 3.3,
         ratingno: 12,
@@ -106,7 +114,7 @@ export const teachers = [
     {
         id: 8,
         name: 'أ/ الزهراء عبد الفتاح محمود',
-        subjects: ["اللغة الفرنسيه", "فيزياء", "التاريخ"],
+        subjects: ["فيزياء", "الكيمياء"],
         grades: ["الصف الخامس", "الصف الرابع", "الصف السابع"],
         rate: 2.3,
         ratingno: 42,
@@ -115,5 +123,161 @@ export const teachers = [
         city: "الرياض",
         location: "التعاون",
         image: teacher8
+    },
+    {
+        id: 9,
+        name: 'أ/محمود عبد المنعم كهربا',
+        subjects: ["اللغة الفرنسية", "فيزياء", "التاريخ"],
+        grades: ["الصف الخامس", "الصف الرابع", "الصف السابع"],
+        rate: 2.3,
+        ratingno: 42,
+        gender: "ذكر",
+        country: "مصر",
+        city: "القاهرة",
+        location: "التعاون",
+        image: teacher9
+    },
+    {
+        id: 10,
+        name: 'أ/ هناء عبد الرحمن',
+        subjects: ["الكيمياء", "الأحياء"],
+        grades: ["الصف السادس", "الصف الثاني عشر"],
+        rate: 4.7,
+        ratingno: 520,
+        gender: "أنثى",
+        country: "الإمارات العربية المتحدة",
+        city: "دبي",
+        location: "المرابع العربية",
+        image: teacher10
+    },
+    {
+        id: 11,
+        name: 'أ/ خالد عبد الله',
+        subjects: ["الرياضيات", "الفيزياء"],
+        grades: ["الصف الأول الثانوي", "الصف العاشر"],
+        rate: 3.9,
+        ratingno: 329,
+        gender: "ذكر",
+        country: "الكويت",
+        city: "الكويت",
+        location: "الفروانية",
+        image: teacher11
+    },
+    {
+        id: 12,
+        name: 'أ/ دينا محمود',
+        subjects: ["اللغة الأنجليزية", "الجغرافيا"],
+        grades: ["الصف السابع", "الصف الثامن"],
+        rate: 4.2,
+        ratingno: 230,
+        gender: "أنثى",
+        country: "مصر",
+        city: "الجيزة",
+        location: "الشيخ زايد",
+        image: teacher12
+    },
+    {
+        id: 13,
+        name: 'أ/ أحمد يوسف',
+        subjects: ["الكيمياء", "الأحياء"],
+        grades: ["الصف العاشر", "الصف الثالث الثانوي"],
+        rate: 4.6,
+        ratingno: 610,
+        gender: "ذكر",
+        country: "الأردن",
+        city: "عمان",
+        location: "الجبيهة",
+        image: teacher13
+    },
+    {
+        id: 14,
+        name: 'أ/ منى علي',
+        subjects: ["الرياضيات", "الفيزياء"],
+        grades: ["الصف الرابع", "الصف التاسع"],
+        rate: 4.5,
+        ratingno: 390,
+        gender: "أنثى",
+        country: "السعودية",
+        city: "جدة",
+        location: "الشاطئ",
+        image: teacher14
+    },
+    {
+        id: 15,
+        name: 'أ/ يحيى محمد',
+        subjects: ["التاريخ", "الجغرافيا"],
+        grades: ["الصف الأول الإعدادي", "الصف الخامس"],
+        rate: 4.8,
+        ratingno: 502,
+        gender: "ذكر",
+        country: "المملكة العربية السعودية",
+        city: "الرياض",
+        location: "الخزامى",
+        image: teacher15
+    },
+    {
+        id: 16,
+        name: 'أ/ ليلى سالم',
+        subjects: ["اللغة العربية", "التاريخ"],
+        grades: ["الصف الثاني عشر", "الصف العاشر"],
+        rate: 4.0,
+        ratingno: 102,
+        gender: "أنثى",
+        country: "البحرين",
+        city: "المنامة",
+        location: "السيف",
+        image: teacher16
+    },
+    {
+        id: 17,
+        name: 'أ/ يوسف الديب',
+        subjects: ["الكيمياء", "الرياضيات"],
+        grades: ["الصف التاسع", "الصف الثامن"],
+        rate: 3.7,
+        ratingno: 480,
+        gender: "ذكر",
+        country: "الأردن",
+        city: "الزرقاء",
+        location: "الزرقاء الجديدة",
+        image: teacher17
+    },
+    {
+        id: 18,
+        name: 'أ/ سهى محمد',
+        subjects: ["اللغة الأنجليزية", "الرياضيات"],
+        grades: ["الصف السابع", "الصف التاسع"],
+        rate: 4.1,
+        ratingno: 322,
+        gender: "أنثى",
+        country: "قطر",
+        city: "الدوحة",
+        location: "الدفنة",
+        image: teacher18
+    },
+    {
+        id: 19,
+        name: 'أ/ حسين عبد القادر',
+        subjects: ["التاريخ", "الفيزياء"],
+        grades: ["الصف العاشر", "الصف الثاني عشر"],
+        rate: 4.5,
+        ratingno: 412,
+        gender: "ذكر",
+        country: "مصر",
+        city: "الإسكندرية",
+        location: "محرم بك",
+        image: teacher19
+    },
+    {
+        id: 20,
+        name: 'أ/ عائشة حسين',
+        subjects: ["الجغرافيا", "العلوم"],
+        grades: ["الصف الخامس", "الصف السابع"],
+        rate: 3.8,
+        ratingno: 50,
+        gender: "أنثى",
+        country: "الإمارات العربية المتحدة",
+        city: "أبوظبي",
+        location: "البطين",
+        image: teacher20
     }
 ]
